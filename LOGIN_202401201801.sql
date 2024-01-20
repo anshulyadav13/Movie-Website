55da08a113a20c7ba2f597ded38e5dfef3a22795
Anshul Yadav <anyadav411998@gmail.com>
2024-01-20 19:04:29 +0530

    dbfiles

---
+++ LOGIN_202401201801.sql
@@ -0,0 +1,7 @@
+INSERT INTO SYSTEM.LOGIN (USERID,PASSWORD,"TYPE") VALUES
+	 ('randomuser','randomuser123','user'),
+	 ('anotherRandomUser','123','user'),
+	 ('admin1','admin1','admin'),
+	 ('anshul','anshul','user'),
+	 ('aman','aman','user'),
+	 ('admin','admin','admin');
---
+++ MOVIECOMMENTS_202401201801.sql
@@ -0,0 +1,37 @@
+INSERT INTO SYSTEM.MOVIECOMMENTS (USERID,TITLE,COMMENTS) VALUES
+	 ('anotherRandomUser','Thor: Love and Thunder','good work'),
+	 ('c','Fantastic Beasts: The Secrets of Dumbledore','nice'),
+	 ('randomuser','Attack on Titan','nice'),
+	 ('randomuser','Demon Slayer: Mugen Train','nyc
+'),
+	 ('randomuser','Jurassic World Dominion','paisa vasool'),
+	 ('user','Thor: Love and Thunder','nice movie'),
+	 ('randomuser','Minions: The Rise of Gru','nice'),
+	 ('randomuser','Thor: Love and Thunder','nice'),
+	 ('randomuser','Spiderhead','nice'),
+	 ('randomuser','The Northman','nice');
+INSERT INTO SYSTEM.MOVIECOMMENTS (USERID,TITLE,COMMENTS) VALUES
+	 ('randomuser','The Bad Guys','bad'),
+	 ('randomuser','Spirited Away','away'),
+	 ('randomuser','Naruto','wow
+'),
+	 ('randomuser','Jurassic World Dominion','six'),
+	 ('randomuser','Haikyu','great'),
+	 ('randomuser','Top Gun: Maverick','top Gun'),
+	 ('admin1','Batman','bat'),
+	 ('anshul','Venom: Let There Be Carnage','nice work'),
+	 ('anshul','Venom:','good job..........'),
+	 ('anshul','Spirited Away','keep it up
+.
+.
+.
+.
+.
+
+
+
+..
+');
+INSERT INTO SYSTEM.MOVIECOMMENTS (USERID,TITLE,COMMENTS) VALUES
+	 ('anshul','Minions: The Rise of Gru','good job'),
+	 ('admin','Spider-Man: No Way Home','very nice');
